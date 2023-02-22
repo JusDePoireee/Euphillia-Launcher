4136f625e15afae826982e0dfb01ade36a100d1f
JusDePoire <antonin@dutreix.net>
2023-02-22 13:47:19 +0100

    Initial

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Dépoussiérage des composants", "author": "JusDePoire" },
+            { "message": "👀", "author": "JusDePoire" },
+            { "message": "Ca arrive", "author": "JusDePoire" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
